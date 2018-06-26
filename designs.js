61185db0f67c7f32c1f002bc4b910cacfe692673
kemiidowu <39502306+kemiidowu@users.noreply.github.com>
2018-06-26 16:18:40 -0700

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,45 @@
+// Select color input
+
+function colorFill(){
+
+      let columnRow= $('td');
+      columnRow.click(function(){
+      let color = $('#colorPicker').val();
+      if ($(this).attr('style')) {
+
+          $(this).removeAttr('style');
+
+      }
+      else {
+
+        $(this).attr('style', 'background-color:' +color);
+      }
+   });
+}
+// When size is submitted by the user, call makeGrid()
+
+$('#sizePicker').submit(function(event){
+    event.preventDefault();
+    let row = $("#inputHeight").val();
+    let column = $('#inputWeight').val();
+    makeGrid(row, column);
+    colorFill(); 
+});
+// Your code goes here!
+function makeGrid(height,width) {
+
+    //$('tr').remove() // remove tr element and its child
+
+    for(let i=1; i<=height; i++){
+         $('#pixelCanvas').append('<tr></tr>'); //add tr 
+    
+        for(let j=1; j<=width; j++){
+            $('tr').filter(':last').append('<td></td>');   // add td to the last td
+        }
+    } 
+}
+
+
+
+
+
