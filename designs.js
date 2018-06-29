983f4ae9dd69562b71e769f9c08cc4279f9c68ad
kemiidowu <39502306+kemiidowu@users.noreply.github.com>
2018-06-29 04:33:51 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,40 +1,77 @@
-// Select color input
+/**
+ * JavaScript
+ * Includes event listners using jquery-3.3.1.min.js library
+ *
+ * Date: 2018-06-29
+ */
+
+// TODO: add background color to the body
+
+$('body').css('background-color', '#cac9cc');
+
+/**
+* @description Select color input on click
+* @val accepts the color selected
+* @attr adds attribute value
+*/
 
 function colorFill(){
 
-      let columnRow = $('td');
-      columnRow.click(function(){
-      let color = $('#colorPicker').val();
-      if ($(this).attr('style')) {          // on click if style is true remove style else add style
+    $('td').click(function() {
 
-          $(this).removeAttr('style');
+        let color = $('.colorPicker').val();
 
-      }
-      else {
+        if ($(this).attr('style')) {
 
-        $(this).attr('style', 'background-color:' +color);
-      }
-   });
-}
-// When size is submitted by the user, call makeGrid()
+            $(this).removeAttr('style');
 
-$('#sizePicker').submit(function(event){
-    event.preventDefault();
-    let row = $("#inputHeight").val();
-    let column = $('#inputWeight').val();
-    makeGrid(row, column);
-    colorFill(); 
-});
-// Your code goes here!
-function makeGrid(height,width) {
-      
-         $('tr').remove(); // remove tr element and its child
-        for(let i=1; i<=height; i++){
-            $('#pixelCanvas').append('<tr></tr>'); //add tr to the table
-    
-        for(let j=1; j<=width; j++){
-            $('tr').filter(':last').append('<td></td>');   // add td to the last td
+        } else {
+
+            $(this).attr('style', 'background-color:' +color);
+
         }
-    } 
- }
+    });
+}
 
+/**
+* @description When size is submitted by the user, call makeGrid() and colorFill()
+* @val {number} inputHeight - accepts the user's value for height as argument for makeGrid()
+* @val {number} inputWeight - accepts the user's value for width as argument for makeGrid()
+*/
+
+$('.sizePicker').submit(function(event) {
+
+    event.preventDefault();
+    let row = $(".inputHeight").val();
+    let column = $('.inputWeight').val();
+    makeGrid(row, column);
+    colorFill();
+
+});
+
+/**
+* @description make n by m grid
+* @param {number} n - The number of rows
+* @param {number} m - The  number of columns
+* @remove removes tr and its child
+* @loop loops the height and width and append the column to the last row
+* @css removes the body background color and adds white background to the table
+*/
+
+function makeGrid(n, m) {
+
+    $('tr').remove();
+
+    for(let i=1; i<=n; i++) {
+
+        $('.pixelCanvas').append('<tr></tr>');
+
+        for(let j=1; j<=m; j++) {
+
+            $('tr').filter(':last').append('<td></td>');
+            $('.pixelCanvas').css('background-color', '');
+            $('.pixelCanvas').css('background-color', 'white');
+
+        }
+    }
+}
